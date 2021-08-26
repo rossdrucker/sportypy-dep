85888cf02b89c52a8457fa55c8b23be50ceafb8a
Ross Drucker <ross.a.drucker@gmail.com>
2021-08-25 21:00:41 -0500

    Added docstring to package __init__

--- sportypy/__init__.py
+++ sportypy/__init__.py
@@ -1,2 +1,9 @@
+"""Draw sports playing surfaces.
+
+This module creates scale representations of various sports playing surfaces
+according to their rulebook specifications.
+
+@author: Ross Drucker
+"""
 __version__ = '0.0.9'
 __author__ = 'Ross Drucker'
